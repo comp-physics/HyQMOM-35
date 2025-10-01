944ff854856a19af19842ae50044f436c9dd91ca
Spencer Bryngelson <sbryngelson@gmail.com>
2025-09-30 22:40:15 -0400

    Refactor: Simplify check2D by collapsing duplicate S11 branches (verified Np=10 tmax=0.1)

--- src/check2D.m
+++ src/check2D.m
@@ -2,26 +2,12 @@ function [S30,S40,S11,S21,S31,S12,S22,S03,S13,S04] = check2D(S30,S40,S11,S21,S31
 % check2D checks and corrects 2D moments in 3D code
 %
 h2min = 1000*eps;
-if S11 >= 1
-    S11 = 1;
+if abs(S11) >= 1
+    % Collapse both S11 >= 1 and S11 <= -1 branches
+    S11 = sign(S11);
     s3m = sqrt(abs(S30*S03));
     s4m = sqrt(S40*S04);
-    H2m = max(h2min,s4m - s3m^2 - 1);
-    s4m = H2m + s3m^2 + 1;
-    S30 = sign(S30)*s3m;
-    S03 = S11*S30;
-    S40 = s4m;
-    S04 = s4m;
-    S12 = S11*S03;
-    S21 = S11*S30;
-    S13 = S11*S04;
-    S31 = S11*S40;
-    S22 = s4m;
-elseif S11 <= -1
-    S11 = -1;
-    s3m = sqrt(abs(S30*S03));
-    s4m = sqrt(S40*S04);
-    H2m = max(h2min,s4m - s3m^2 - 1);
+    H2m = max(h2min, s4m - s3m^2 - 1);
     s4m = H2m + s3m^2 + 1;
     S30 = sign(S30)*s3m;
     S03 = S11*S30;
