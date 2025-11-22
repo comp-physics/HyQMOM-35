3d93bd242146ec7b7756611e97731ff47bb1fcf3
Spencer Bryngelson <sbryngelson@gmail.com>
2025-11-21 21:27:27 -0500

    Add dummy Sphinx master document for ReadTheDocs
    ReadTheDocs runs Sphinx before post_build jobs. Created minimal
    nonexistent.rst so Sphinx can complete successfully.
    
    The Sphinx build creates minimal HTML, then post_build runs and
    Documenter.jl overwrites it with the actual documentation.
    
    Updated conf.py to exclude markdown files and Julia source from Sphinx.

--- docs/conf.py
+++ docs/conf.py
@@ -1,17 +1,18 @@
 # Minimal Sphinx configuration for ReadTheDocs
-# This is required by ReadTheDocs but not used
+# This is required by ReadTheDocs but not actually used
 # Actual documentation is built using Julia's Documenter.jl in post_build
+# The post_build job will overwrite this Sphinx output with Documenter.jl HTML
 
 project = 'HyQMOM.jl'
 copyright = '2024, Computational Physics Group'
 author = 'Computational Physics Group'
 
-# Point to a non-existent master doc so Sphinx has nothing to build
+# Minimal master doc (will be overwritten by Documenter.jl)
 master_doc = 'nonexistent'
 
 html_theme = 'alabaster'
 extensions = []
 
-# Exclude all patterns to prevent Sphinx from processing anything
-exclude_patterns = ['*']
+# Exclude markdown and Julia files from Sphinx
+exclude_patterns = ['*.md', 'src/**', 'make.jl', 'Project.toml', 'Manifest.toml', 'build/**']
 
